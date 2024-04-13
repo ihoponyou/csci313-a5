93b434ecc63a616fa9c175028f241ada03fefa91
noahmagcalas <noah.magcalas@gmail.com>
2024-04-13 14:27:41 -0500

    ngmodel shenanigans

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
+  standalone: true,
+  imports: [
+    NgFor,
+    NgIf,
+    UpperCasePipe
+  ]
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
