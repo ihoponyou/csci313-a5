c8d64a6a55240364d96318632af2bb89ca25cb51
noahmagcalas <noah.magcalas@gmail.com>
2024-04-14 19:32:14 -0500

    actually fixed imports

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
-  standalone: true,
-  imports: [
-    FormsModule,
-    NgFor,
-    NgIf,
-    UpperCasePipe
-  ]
 })
 export class HeroesComponent {
   heroes = HEROES;
