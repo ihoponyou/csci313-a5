dd1a81d5399994a190887a0eb75169ac006ef062
noahmagcalas <noah.magcalas@gmail.com>
2024-04-27 18:30:17 -0500

    edit heroes component template & css

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -100,7 +100,7 @@ export class HeroService {
     }
     return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
       tap(x => x.length ?
-        this.log(`found heroes matching "${term}"`) :
+        this.log(`found ${x.length} ${x.length > 1 ? "heroes" : "hero"} matching "${term}"`) :
         this.log(`no heroes matching "${term}"`)),
       catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
